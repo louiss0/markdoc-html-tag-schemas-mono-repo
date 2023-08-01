709dfa845a8f5c2e24b225256672d41256d0e991
shelton louis <louisshelton0@gmail.com>
2023-08-01 23:06:48 +0000

    fix: I'm using the getter

--- packages/markdoc-html-tag-schemas/src/lib/schema/tag/abbreviation.ts
+++ packages/markdoc-html-tag-schemas/src/lib/schema/tag/abbreviation.ts
@@ -1,12 +1,11 @@
 
-import type { ProperSchemaMatches, RequiredSchemaAttribute } from "packages/markdoc-html-tag-schemas/src/lib/attributes";
-import { createAnArrayOfMarkdocErrorObjectsBasedOnEachConditionThatIsTrue, generateMarkdocErrorObjectThatHasAMessageThatTellsTheUserAValueIsNotRight, generateSelfClosingTagSchema } from "packages/markdoc-html-tag-schemas/src/utils";
+import { createAnArrayOfMarkdocErrorObjectsBasedOnEachConditionThatIsTrue, generateMarkdocErrorObjectThatHasAMessageThatTellsTheUserAValueIsNotRight, getGenerateSelfClosingTagSchema } from "packages/markdoc-html-tag-schemas/src/utils";
 
 
 const matchCapitalisedWordCaptureOtherCapitalizedWordsOnOneLineRegex =
     /^[A-Z][a-z]*(\s[A-Z][a-z]*)*$/;
 // const isAnUppercasedWord = /^[A-Z]+/
-export const abbr = generateSelfClosingTagSchema<ProperSchemaMatches, RequiredSchemaAttribute, "abbr">(
+export const abbr = getGenerateSelfClosingTagSchema()(
     {
         render: "abbr",
         type: String,
@@ -44,10 +43,10 @@ export const abbr = generateSelfClosingTagSchema<ProperSchemaMatches, RequiredSc
                         `You are supposed to supply only words that are capitalised with spaces
                          when using the primary attribute.
                          If you want to write an abbreviation in the primary attribute
-                         use the title attribute along with it and put the 
+                         use the title attribute along with it and put the
                          capitalised word there.
                          Please make sure to place the abbreviation as the primary attribute
-                         if you do. 
+                         if you do.
                          This word ${primary} doesn't meet that condition.
                         `
                     )
