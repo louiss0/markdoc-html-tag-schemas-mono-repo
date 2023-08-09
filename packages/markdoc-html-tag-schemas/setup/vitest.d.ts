fb2fecb18aedf5e0a10af148f862c5be429f4d09
louiss0 <louisshelton0gmail.com>
2023-08-08 21:04:51 -0400

    chore; Finish mocking the app

--- packages/markdoc-html-tag-schemas/setup/vitest.d.ts
+++ packages/markdoc-html-tag-schemas/setup/vitest.d.ts
@@ -2,13 +2,14 @@ import type { Assertion, TestAPI } from 'vitest'
 
 declare module "vitest" {
 
-    interface CustomMatchers<T extends unknown> {
+    interface CustomMatchers<T> {
         toEqualMarkdocErrorObject(): T
         toEqualMarkdocErrorObjectThatTellsTheUserThatATypeIsNotRight(): T
         toEqualMarkdocErrorObjectThatTellsTheUserThatAValueIsNotRight(): T
 
     }
 
+    // eslint-disable-next-line @typescript-eslint/no-empty-interface
     interface Assertion<T = unknown> extends CustomMatchers<T> { }
 
 }
