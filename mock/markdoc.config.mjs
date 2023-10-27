0c0380bab12dbb3f587ffb869239f37749d4986d
louiss0 <louisshelton0gmail.com>
2023-10-27 12:25:01 -0400

    refactor: Use spread operator for markdoc-html-tag-schemas

--- mock/markdoc.config.mjs
+++ mock/markdoc.config.mjs
@@ -1,9 +1,13 @@
 import { defineMarkdocConfig } from '@astrojs/markdoc/config';
 
 import { markdocHTMLTagSchemas } from 'markdoc-html-tag-schemas';
+
 const { nodes, tags } = markdocHTMLTagSchemas();
+
 export default defineMarkdocConfig({
-  nodes,
+  nodes: {
+    ...nodes
+  },
   tags: {
     ...tags,
   },
