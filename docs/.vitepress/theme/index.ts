14360f6c79e517ac47f8c5661af74366e67396fb
louiss0 <louisshelton0gmail.com>
2023-11-08 15:30:39 -0500

    feat: Implement the attribute table.

--- docs/.vitepress/theme/index.ts
+++ docs/.vitepress/theme/index.ts
@@ -1,8 +1,11 @@
 import type { Theme } from 'vitepress';
 import DefaultTheme from 'vitepress/theme';
-
+import AttributeTable from '../../components/AttributeTable.vue';
 import Default from './Default.vue';
 export default {
-    ...DefaultTheme,
-    Layout: Default
+    extends: DefaultTheme,
+    Layout: Default,
+    enhanceApp(ctx) {
+        ctx.app.component("AttributeTable", AttributeTable)
+    }
 } satisfies Theme
---
+++ docs/components/AttributeTable.vue
@@ -0,0 +1,87 @@
+<script setup lang="ts">
+type Attribute =  {
+      attribute: string;
+      type: {
+        href: string;
+        text: string;
+      } | string ;
+      required?: "true";
+      errorLevel?:string;
+      reference?:string;
+      matches?: Array<string> | undefined;
+    };
+
+const {attributeList} = defineProps<{
+  attributeList: Array<Attribute>;
+}>();
+
+const someValuesAreErrorLevels = attributeList
+  .some(attribute => "errorLevel" in attribute )
+
+const someValuesAreReferences = attributeList
+  .some(attribute => "reference" in attribute )
+
+</script>
+
+<template>
+  <table>
+    <caption>
+      Attributes
+    </caption>
+
+    <thead>
+      <th>attribute</th>
+      <th>type</th>
+      <th>required</th>
+      <th v-if="someValuesAreErrorLevels">error level</th>
+      <th v-if="someValuesAreReferences">ref</th>
+      <th>matches</th>
+    </thead>
+
+    <tbody>
+      <template
+        v-for="{ attribute, type, required='false',
+          reference,
+         errorLevel, matches } of attributeList"
+        :key="attribute"
+      >
+        <tr>
+          <td>{{ attribute }}</td>
+
+          <template v-if="typeof type === 'string'">
+            <td>{{ type }}</td>
+          </template>
+
+          <template v-else>
+            <td>
+              <a :href="type.href">
+                {{ type.text }}
+              </a>
+            </td>
+          </template>
+
+          <td>{{ required }}</td>
+
+          <td v-if="someValuesAreErrorLevels">
+            
+            {{ errorLevel }}
+          
+          </td>
+          <td v-if="someValuesAreReferences">
+            
+            {{ reference }}
+          
+          </td>
+
+          <td>
+            <template v-if="matches">
+              <template v-for="match of matches">
+                <div>{{ match }}</div>
+              </template>
+            </template>
+          </td>
+        </tr>
+      </template>
+    </tbody>
+  </table>
+</template>
